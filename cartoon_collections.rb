d6b66ea356c6473b1b5b93217ed4e1129e8af3de
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-23 23:14:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,6 +31,10 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+=begin
 .each do |color|
   puts "There are #{color.length} letters in #{color}"
 end #=> ["Red", "Yellow", "Blue"]
+
+
