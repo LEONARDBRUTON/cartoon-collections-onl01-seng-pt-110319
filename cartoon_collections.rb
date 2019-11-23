c44f1621493a6e26e57dc1b5286dbae2ca4e1797
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-23 23:55:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,25 +27,11 @@ def long_planeteer_calls(calls)
       return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese("array")
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
-=begin
-long_planteer_calls.each do |call|
-    if call.length > 4
-      answer = true
-    end
-  end
-    answer
-end
 
 
 
-=begin
-.each do |color|
-  puts "There are #{color.length} letters in #{color}"
-end #=> ["Red", "Yellow", "Blue"]
 
-
-=end
